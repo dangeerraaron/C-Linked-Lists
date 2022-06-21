5eace286f202f3a45a5c666548b498821d1dd981
Aaron J Lanning <64545232+dangeerraaron@users.noreply.github.com>
2022-06-20 22:42:02 -0400

    Add files via upload
    display node using recursion

---
+++ RecursionLinkedList.C
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+//linked list practice/template
+struct Node
+{
+int data;
+struct Node *next;
+
+}*first = NULL; //global pointer, init NULL
+
+void CreateNode(int A[], int n)
+{
+   int i=0;
+   struct Node *t, *last;
+   first=(struct Node *)malloc(sizeof(struct Node)); //creating the first node, first pointing at it
+   first->data = A[0]; //array index 0 assigned to node
+   first->next = NULL; //assign null to terminal node
+   last=first; //first and last node point to NULL
+   
+   for(i=1; i<n; i++) //for loop to store array elements in linked list, start from '1' as first node already created
+   {
+      t=(struct Node *)malloc(sizeof(struct Node)); //create a new node
+      t->data=A[i]; //assign index to data of node struct
+      t->next=NULL; //have next pointer to null, as no new node yet
+      last->next=t; // last node points at t
+      last=t; //t node becomes new node
+   }   
+
+}
+void DisplayNode(struct Node *p)
+{
+   while(p != NULL) //iterate until null pointer
+   {
+      printf("%d ", p->data); //pointer to data in struct note, to print
+      p=p->next; //move pointer to next node
+   }
+   
+   printf("\n");
+}
+int RDisplayTail(struct Node *p) //tail recursion
+{
+    if(p != NULL)
+    {
+        printf("%d ", p->data); //print data stored in address of first node
+        RDisplayTail(p->next); //call function to the next node
+    }
+    printf("\n");
+}
+int RDisplayHead(struct Node *p) //head recursion
+{
+   if(p != NULL)
+   {
+       RDisplayHead(p->next); //function call to next node
+       printf("%d ",p->data); //print linked List IN REVERSE
+   }
+} 
+int main(void)
+{
+  int A[5]={3,5,7,10,15};
+  
+  CreateNode(A,5);
+  DisplayNode(first); //passes first global pointer to display function, accepts parameter as *p
+  
+  RDisplayTail(first);
+  RDisplayHead(first);
+  
+    return 0;
+}
